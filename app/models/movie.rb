84b70b6484d18aec0b3000ec62253281b234a2b3
jason <wjcodes@example.com>
2020-08-13 21:09:28 -0400

    first commit

---
+++ app/models/movie.rb
@@ -0,0 +1,34 @@
+class Movie < ApplicationRecord
+
+  has_many :reviews, dependent: :destroy
+
+  validates :title, :released_on, :duration, presence: true
+
+  validates :description, length: { minimum: 25 }
+
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+  validates :rating, inclusion: { in: RATINGS }
+
+  def self.released
+    where("released_on < ?", Time.now).order("released_on desc")
+  end
+  
+  def flop?
+    total_gross.blank? || total_gross < 225_000_000
+  end
+
+  def average_stars
+    reviews.average(:stars) || 0.0
+  end
+
+  def average_stars_as_percent
+    (average_stars / 5.0) * 100
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :users
+  root "movies#index"
+
+  resources :movies do
+    resources :reviews
+  end
+end
